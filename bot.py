bc6652e7aa6cfad6679ef5460f1f81ee12cb5c8d
yfsanchez <yfsanchez@gmail.com>
2022-01-24 15:35:29 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,20 @@ def start(update, context):
 
     update.message.reply_text('Hola humano')
 
+def help(update, context):
+
+    update.message.reply_text('Help to play')
+
 if __name__ == '__main__':
 
-    updater = Updater(token='1784474668:AAEpxVEtCs_SMYBijeOSIoCVuSy5gAGoOyg', use_context=True)
+    updater = Updater(token='1784474668:AAG6DffL1G2VBkzzMH1X74ZEBQGsii9i8-Y', use_context=True)
 
     dp = updater.dispatcher
 
     #add handler
     dp.add_handler(CommandHandler('start', start))
+
+    dp.add_handler(CommandHandler('help', help))
 
     updater.start_polling()
     updater.idle()
